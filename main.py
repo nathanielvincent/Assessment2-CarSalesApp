6ebe6b34ccf4792cce557497c343cc80a2c6082e
nathanielvincent <nav0135@arastudent.ac.nz>
2026-09-17 10:47:45 +1200

    Add sales data file path validation

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 # nav0135@arastudent.ac.nz
 # A program to read car sale data from a .csv
 
+import os
+
 def load_sales(filename: str) -> list:
     """
     Loads all sales information from the provided file path and returns a list
@@ -10,6 +12,13 @@ def load_sales(filename: str) -> list:
     :param filename: the path to the sales data csv.
     :return: a list of dictionaries with sales data.
     """
+
+    if not os.path.exists(filename):
+        # This should raise a ValueError, but the assessment requires
+        # this approach, so ignore the (rightful) type checker complaints.
+        print('Sales data file path does not exist.')
+        return
+
     with open(filename, 'r') as file:
         lines       = file.readlines()
         csv_keys    = lines[0].strip().split(",") # Build the CSV header keys
@@ -26,6 +35,7 @@ def load_sales(filename: str) -> list:
                 car_sales_cache_dict[csv_keys[i]] = entry_data # Add the data to the cache dictionary
 
             car_sales.append(car_sales_cache_dict) # Add the cache dictionary to the car sales list.
+
         return car_sales
 
 if __name__ == '__main__':
