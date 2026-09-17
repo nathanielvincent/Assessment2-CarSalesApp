e9257b046ae68582f44ea290af27ea17e36e2fc5
nathaniel vincent <nav0135@arastudent.ac.nz>
2026-09-17 17:17:22 +1200

    Added 'Best Month' option. Added options as a list to begin migration away from needless dictionary.

--- constants.py
+++ constants.py
@@ -8,5 +8,19 @@ MENU_OPTIONS = {
     "Average Sale by Salesperson": "average_sale_by_salesperson",
     "Top Salesperson by Average": "top_salesperson_by_average",
     "Sales in Month": "sales_in_month",
+    "Best Month": "best_month",
     "Quit": "quit"
 }
+
+MENU_OPTIONS_LIST = [
+    "Total Sales",
+    "Total Revenue",
+    "Sales by Salesperson",
+    "Most Sold Make",
+    "Least Sold Model",
+    "Average Sale by Salesperson",
+    "Top Salesperson by Average",
+    "Sales in Month",
+    "Best Month",
+    "Quit",
+]
