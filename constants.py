d5fc8f0f6561aa87ccdffd99ca699ed5c11e3324
nathanielvincent <nav0135@arastudent.ac.nz>
2026-09-17 14:45:23 +1200

    Move data to constants that multiple files will rely on.

---
+++ constants.py
@@ -0,0 +1,9 @@
+# Options that will appear on startup for the interaction menu.
+MENU_OPTIONS = {
+    "Total Sales": "total_sales",
+    "Total Revenue": "total_revenue",
+    "Sales by Salesperson": "sales_by_salesperson",
+    "Most Sold Make": "most_sold_make",
+    "Least Sold Model": "least_sold_model",
+    "Quit": "quit"
+}
--- main.py
+++ main.py
@@ -3,16 +3,7 @@
 # A program to read car sale data from a .csv
 
 from analytics_grabber import grab_analytics
-
-# Options that will appear on startup for the interaction menu.
-MENU_OPTIONS = {
-    "Total Sales": "total_sales",
-    "Total Revenue": "total_revenue",
-    "Sales by Salesperson": "sales_by_salesperson",
-    "Most Sold Make": "most_sold_make",
-    "Least Sold Model": "least_sold_model",
-    "Quit": "quit"
-}
+from constants import *
 
 def sales_interaction_menu() -> None:
     """
