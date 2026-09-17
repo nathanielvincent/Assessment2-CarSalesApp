9ba6d79ff404719019c5ac2b4b2a0a2393b69f3c
nathanielvincent <nav0135@arastudent.ac.nz>
2026-09-17 12:07:48 +1200

    Move analytics to dedicated file for readability

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # A program to read car sale data from a .csv
 
 import os
+from analytics import *
 
 def parse_value(value):
     """
@@ -21,96 +22,6 @@ def parse_value(value):
     except ValueError:
         return value
 
-
-# ANALYTICS FUNCTIONS --------------------------------------------------------
-def total_sales(sales: list) -> int:
-    """
-    Returns the total count of sales from the provided data.
-
-    :param sales: List data gathered from `load_sales`.
-    :return: Integer count of sales.
-    """
-    return len(sales)
-
-def total_revenue(sales: list) -> float:
-    """
-    Returns the total revenue from sales.
-
-    :param sales: List data gathered from `load_sales`.
-    :return: Float of total revenue count.
-    """
-
-    revenue_total = 0
-    for item in sales:
-        try:
-            price = item['price']
-            revenue_total += price
-
-        except: # Malformed entry, ignore, move on.
-            continue
-
-    return float(revenue_total)
-
-
-def sales_by_salesperson(sales: list) -> dict:
-    """
-    Generates a dictionary of sales person sales stats
-
-    :param sales: List data gathered from `load_sales`.
-    :return: a dictionary of all salespeople and how many sales they have made.
-    """
-
-    sales_person_stats = {}
-    for sale in sales:
-        try:
-            sales_person = sale['salesperson']
-
-            # Add salesperson to dictionary if they don't exist
-            if sales_person not in sales_person_stats:
-                sales_person_stats[sales_person] = 1
-                continue
-
-            sales_person_stats[sales_person] += 1
-
-        except: # Malformed entry, ignore, move on.
-            continue
-
-    return sales_person_stats
-
-def most_sold_make(sales: list) -> str:
-    """
-
-    :param sales: List data gathered from `load_sales`.
-    :return:
-    """
-
-    car_sales_stats = {}
-    for sale in sales:
-        try:
-            car_make = sale['make']
-
-            # Add salesperson to dictionary if they don't exist
-            if car_make not in car_sales_stats:
-                car_sales_stats[car_make] = 1
-                continue
-
-            car_sales_stats[car_make] += 1
-
-        except: # Malformed entry, ignore, move on.
-            continue
-
-    most_sold_make_count = 0
-    best_sold_make       = ""
-    for make in car_sales_stats:
-        # Check if car make sales is more than already stored most sold count
-        if car_sales_stats[make] > most_sold_make_count:
-            most_sold_make_count = car_sales_stats[make]
-            best_sold_make = make
-
-    return best_sold_make
-
-
-# LOADING DATA --------------------------------------------------------
 def load_sales(filename: str) -> list:
     """
     Loads all sales information from the provided file path and returns a list
