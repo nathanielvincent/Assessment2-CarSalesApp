750a8af3d3408ce639b66ee3fdd6ed052fc710f6
nathanielvincent <nav0135@arastudent.ac.nz>
2026-09-17 14:27:21 +1200

    More bug squashing, moving functions over to analytics_grabber.py

--- analytics_grabber.py
+++ analytics_grabber.py
@@ -1,5 +1,56 @@
-from main import load_sales
 from analytics import *
+import os
+
+def parse_value(value):
+    """
+    Parses the given value and will convert it to an int, float, or just return the normal value
+
+    :param value: data to check the type.
+    :return: modified data, either an int, float, or value.
+    """
+
+    if value.isdecimal():
+        return int(value)
+
+    try:
+        return float(value)
+
+    except ValueError:
+        return value
+
+def load_sales(filename: str) -> list:
+    """
+    Loads all sales information from the provided file path and returns a list
+    with formatted data entries using respective key headers.
+
+    :param filename: the path to the sales data csv.
+    :return: a list of dictionaries with sales data.
+    """
+
+    if not os.path.exists(filename):
+        # Don't throw python errors at the user, handle errors safely.
+        print('Sales data file path does not exist.')
+        return
+
+    with open(filename, 'r') as file:
+        lines       = file.readlines()
+        csv_keys    = lines[0].strip().split(",") # Build the CSV header keys
+        car_sales   = []
+
+        for line in lines[1:]:
+            car_sales_cache_dict = {} # Set the cache dict here so it gets reset with every run of the loop.
+            split_sales_data     = line.strip().split(",") # Split the keys from the line.
+
+            for i in range(len(csv_keys)):
+                # Convert the data into integers if possible.
+                # TODO: Error handling when malformed data
+                entry_data = parse_value(split_sales_data[i])
+
+                car_sales_cache_dict[csv_keys[i]] = entry_data # Add the data to the cache dictionary
+
+            car_sales.append(car_sales_cache_dict)  # Add the cache dictionary to the car sales list.
+
+        return car_sales
 
 def grab_analytics(run_option: str) -> None:
     """
--- main.py
+++ main.py
@@ -16,57 +16,6 @@ MENU_OPTIONS = {
     "Quit": "quit"
 }
 
-def parse_value(value):
-    """
-    Parses the given value and will convert it to an int, float, or just return the normal value
-
-    :param value: data to check the type.
-    :return: modified data, either an int, float, or value.
-    """
-
-    if value.isdecimal():
-        return int(value)
-
-    try:
-        return float(value)
-
-    except ValueError:
-        return value
-
-def load_sales(filename: str) -> list:
-    """
-    Loads all sales information from the provided file path and returns a list
-    with formatted data entries using respective key headers.
-
-    :param filename: the path to the sales data csv.
-    :return: a list of dictionaries with sales data.
-    """
-
-    if not os.path.exists(filename):
-        # Don't throw python errors at the user, handle errors safely.
-        print('Sales data file path does not exist.')
-        return
-
-    with open(filename, 'r') as file:
-        lines       = file.readlines()
-        csv_keys    = lines[0].strip().split(",") # Build the CSV header keys
-        car_sales   = []
-
-        for line in lines[1:]:
-            car_sales_cache_dict = {} # Set the cache dict here so it gets reset with every run of the loop.
-            split_sales_data     = line.strip().split(",") # Split the keys from the line.
-
-            for i in range(len(csv_keys)):
-                # Convert the data into integers if possible.
-                # TODO: Error handling when malformed data
-                entry_data = parse_value(split_sales_data[i])
-
-                car_sales_cache_dict[csv_keys[i]] = entry_data # Add the data to the cache dictionary
-
-            car_sales.append(car_sales_cache_dict)  # Add the cache dictionary to the car sales list.
-
-        return car_sales
-
 def sales_interaction_menu() -> None:
     """
     Runs a menu of data analysis options until the user requests a quit.
@@ -103,15 +52,3 @@ def sales_interaction_menu() -> None:
 
 if __name__ == '__main__':
     sales_interaction_menu()
-
-    # sale_data = load_sales('./car_sales.csv')
-    #
-    # least_sold  = least_sold_model(sale_data)
-    # most_sold   = most_sold_make(sale_data)
-    # sales_count = total_sales(sale_data)
-    # revenue     = total_revenue(sale_data)
-    #
-    # print(f"Least Sold Model: {least_sold}")
-    # print(f"Most Sold Make  : {most_sold}")
-    # print(f"Total Sales     : {sales_count}")
-    # print(f"Total Revenue   : {revenue}")
