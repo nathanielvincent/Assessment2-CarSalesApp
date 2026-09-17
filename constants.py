111b60ded559bf54b04b9c13cc7b50ebe0c3a382
nathanielvincent <nav0135@arastudent.ac.nz>
2026-09-18 00:59:26 +1200

    Cleaning up root directory

--- constants.py
+++ constants.py
@@ -2,12 +2,14 @@
 # nav0135@arastudent.ac.nz
 # Constant variables and functions that multiple files depend on, so they're here instead
 # of being duplicated multiple times through the source.
+
 import os
+from pathlib import Path
 
 # This is the file that will be utilised by the code for all functions.
 # If you wish to change the file it reads, change this, and everything will use it.
-WORKING_DATA_FILE = './car_sales.csv'
-
+WORKING_DIR = Path(__file__).resolve().parent
+WORKING_DATA_FILE = WORKING_DIR / 'Data' / 'car_sales.csv'
 
 # Options that will appear on startup for the interaction menu in main.py.
 # This is also used for analytics_grabber.py to know what code to run.
