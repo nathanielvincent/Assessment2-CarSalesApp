77fb53b78a86f4c3bcc9c68f07d8b200bc247fcb
nathaniel vincent <nav0135@arastudent.ac.nz>
2026-09-18 17:19:56 +1200

    Print a pretty table thats easier to read.

--- analytics_grabber.py
+++ analytics_grabber.py
@@ -129,13 +129,14 @@ def grab_analytics(run_option: str, csv_file_path: str) -> None:
 
             else:
                 print(f"Here's a list of sales made in {year}-{month}:")
+                print(f"{'Salesperson':<15}{'Model':<25}{'Price':>10}")
 
                 for entry in month_data:
                     salesperson = entry['salesperson']
                     car = f"{entry['make']} {entry['model']}"
                     price = f"${entry['price']:.2f}"
 
-                    print(f"{salesperson} sold a {car} for {price}.")
+                    print(f"{salesperson:<15}{car:<25}{price:>10}")
         else:
             print("\n!! One or more input is incorrect, please try again !!")
 
