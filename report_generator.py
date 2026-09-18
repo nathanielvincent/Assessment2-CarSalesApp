319a716771d9ea5e70db599aa5580532fee2341a
nathaniel vincent <nav0135@arastudent.ac.nz>
2026-09-18 16:32:47 +1200

    Improve report generator

--- report_generator.py
+++ report_generator.py
@@ -26,13 +26,16 @@ def write_report(sales: list, filename: str) -> None:
     average_salesperson_sales   = average_sale_by_salesperson(sales)
     top_salesperson_average     = top_salesperson_by_average(sales)
 
+    best_month_data = best_month(sales)
+
     with open(filename, 'w') as file:
         # General Stats ---------------------------
         file.write(f'VEHICLE SALES STATS:\n')
         file.write(f'Total car sales in the dataset       - {total_sales_amount}\n')
-        file.write(f'Total Revenue from all sales         - ${total_revenue_amount:.2f}\n\n')
+        file.write(f'Total revenue from all sales         - ${total_revenue_amount:.2f}\n\n')
         file.write(f'The overall most sold vehicle make   - {most_sold_car_make}.\n')
-        file.write(f'The overall least sold vehicle model - {least_sold_car_model}.\n\n\n')
+        file.write(f'The overall least sold vehicle model - {least_sold_car_model}.\n')
+        file.write(f'The best month in the data           - {best_month_data}\n\n\n')
 
         # Total sale count ---------------------------
         file.write(f'SALES PEOPLE STATS:\n')
