6c7a494522de4941d78decb2462570906d6a80c6
nathaniel vincent <nav0135@arastudent.ac.nz>
2026-09-18 17:16:47 +1200

    Removed TODO, to be honest I did this poorly, but I have 2 months to rework it if I care to.

--- analytics_grabber.py
+++ analytics_grabber.py
@@ -52,7 +52,6 @@ def load_sales(filename: str) -> list:
 
             for i in range(len(csv_keys)):
                 # Convert the data into integers if possible.
-                # TODO: Error handling when malformed data
                 entry_data = parse_value(split_sales_data[i])
 
                 car_sales_cache_dict[csv_keys[i]] = entry_data # Add the data to the cache dictionary
